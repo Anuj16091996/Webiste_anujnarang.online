52685c4a556cedff4a5384227022f8e0408385f3
Anuj16091996 <anuj161996@gmail.com>
2022-03-18 22:47:47 -0400

    Created the underconstruction page

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+
+<html>
+<head>
+<title>Portfolio</title>
+<link href="CSS/style.css" rel="stylesheet" type="text/css">
+    <link rel="shortcut icon" type="image/jpg" href="View/Images/ALogo-removebg-preview.png">
+</head>
+
+
+<body>
+<div class="Under_Construction">
+
+</div>
+</body>
+
+</html>
